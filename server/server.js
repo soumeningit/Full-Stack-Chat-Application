e98c3cdcf8c63ebd2c8bdc05862960c5388622c0
soumeningit <soumenpalcs@gmail.com>
2024-09-05 01:24:39 +0530

    Updated package.json and server.js

--- server/server.js
+++ server/server.js
@@ -190,15 +190,10 @@ app.use("/api/chat", updateUserRoute);
 const __dirname1 = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-    // app.use(express.static(path.join(__dirname1, "/client/build")));
-    app.use(express.static(path.join(__dirname, 'client/build')));
-
-    // app.get("*", (req, res) =>
-    //     res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"))
-    // );
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-    });
+    app.use(express.static(path.join(__dirname1, "/client/build")));
+    app.get("*", (req, res) =>
+        res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"))
+    );
 } else {
     app.get("/", (req, res) => {
         return res.json({

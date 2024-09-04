7fa241f55e79b669203194d83ad550a70193553b
soumeningit <soumenpalcs@gmail.com>
2024-09-05 01:03:14 +0530

    Updated package.json and server.js

--- server/server.js
+++ server/server.js
@@ -193,9 +193,12 @@ if (process.env.NODE_ENV === "production") {
     // app.use(express.static(path.join(__dirname1, "/client/build")));
     app.use(express.static(path.join(__dirname, 'client/build')));
 
-    app.get("*", (req, res) =>
-        res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"))
-    );
+    // app.get("*", (req, res) =>
+    //     res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"))
+    // );
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
 } else {
     app.get("/", (req, res) => {
         return res.json({
